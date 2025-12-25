311c1606af50a50845a19a74a43fab2eadcef7cb
Siraj <sirajshabbir321@gmail.com>
2025-12-25 18:53:23 +0500

    add a vercel allowed origin

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080", "http://localhost:8002", "http://localhost:3000"],
+    allow_origins=["http://localhost:8080", "http://localhost:8002", "http://localhost:3000", "hex-teams-frontend.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
