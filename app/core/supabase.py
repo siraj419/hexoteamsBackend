394e860edca83e067ee624ed39900abacfc05bb3
Siraj <sirajshabbir321@gmail.com>
2026-03-10 13:15:18 +0500

    add debug logs

--- app/core/supabase.py
+++ app/core/supabase.py
@@ -4,8 +4,11 @@ from .config import Settings
 
 settings = Settings()
 
-_service_key = settings.SUPABASE_SERVICE_ROLE_KEY.strip().strip('"').strip("'")
-_anon_key = settings.SUPABASE_KEY.strip().strip('"').strip("'")
+service_key = settings.SUPABASE_SERVICE_ROLE_KEY
+anon_key = settings.SUPABASE_KEY
+
+print("anon_key: ", anon_key)
+print("service_key: ", service_key)
 
 supabase: Client = create_client(settings.SUPABASE_URL, _service_key)
 supabase_auth_client: Client = create_client(settings.SUPABASE_URL, _anon_key)
