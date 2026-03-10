8187f2edd62f8caee5ce26a9b7e929428aeccf0e
Siraj <sirajshabbir321@gmail.com>
2026-03-10 13:15:46 +0500

    fix reference errors

--- app/core/supabase.py
+++ app/core/supabase.py
@@ -7,8 +7,8 @@ settings = Settings()
 service_key = settings.SUPABASE_SERVICE_ROLE_KEY
 anon_key = settings.SUPABASE_KEY
 
-print("anon_key: ", anon_key)
-print("service_key: ", service_key)
+print("ANON_KEY: ", anon_key)
+print("SERVICE_KEY: ", service_key)
 
-supabase: Client = create_client(settings.SUPABASE_URL, _service_key)
-supabase_auth_client: Client = create_client(settings.SUPABASE_URL, _anon_key)
+supabase: Client = create_client(settings.SUPABASE_URL, service_key)
+supabase_auth_client: Client = create_client(settings.SUPABASE_URL, anon_key)
